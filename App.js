480213d29ae92f7e7d2caaa7f47058fd45803d00
cpodimas@gmail.com <cpodimas@gmail.com>
2020-01-08 23:03:28 +0300

    auto

--- App.js
+++ App.js
@@ -18,15 +18,22 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-import { Endpoint } from 'react-native-tele'
+import { Endpoint as teleEndpoint } from 'react-native-tele'
+import { Endpoint as sipEndpoint } from 'react-native-sip'
 import { ReplaceDialer } from 'react-native-replace-dialer'
 
 export default class App extends Component {
   constructor() {
     super();
   }
+  
   async componentDidMount() {
+    await tEndpointInit();
+    await sEndpointInit();
+  }
 
+  async tEndpointInit() {
+    console.log("tEndpointInit()");
     let tReplaceDialer = new ReplaceDialer();
 
     if (!tReplaceDialer.isDefault()) {
@@ -39,20 +46,22 @@ export default class App extends Component {
     }
 
 
-    let tEndpoint = new Endpoint();
-    console.log(tEndpoint);
+    this.tEndpoint = new teleEndpoint();
+    //console.log(this.tEndpoint);
 
-    let state = await tEndpoint.start(); // List of calls when RN context is started, could not be empty because Background service is working on Android
+    let state = await this.tEndpoint.start(); // List of calls when RN context is started, could not be empty because Background service is working on Android
+    console.log("sEndpoint started");
+
     let { calls, settings } = state;
-    console.log("calls",calls);
-    console.log("settings",settings);
+    console.log("calls:\n", calls);
+    console.log("settings:\n", settings);
 
     // Subscribe to endpoint events
     // tEndpoint.on("registration_changed", (account) => {}); // TODO
     // tEndpoint.on("connectivity_changed", (online) => {}); // TODO
-    tEndpoint.on("call_received", (call) => { console.log("call_received",call); });
-    tEndpoint.on("call_changed", (call) => { console.log("call_changed",call);  });
-    tEndpoint.on("call_terminated", (call) => { console.log("call_terminated",call);  });
+    this.tEndpoint.on("call_received", (call) => { console.log("call_received",call); });
+    this.tEndpoint.on("call_changed", (call) => { console.log("call_changed",call);  });
+    this.tEndpoint.on("call_terminated", (call) => { console.log("call_terminated",call);  });
     // tEndpoint.on("call_screen_locked", (call) => {  console.log("call_screen_locked",call);  }); // Android only
 
 
@@ -63,6 +72,125 @@ export default class App extends Component {
     }
 
     //let call = await tEndpoint.makeCall(destination, options);
+  }
+
+  async sEndpointInit() {
+    console.log("sEndpointInit()");
+    this.sEndpoint = new sipEndpoint;
+
+    let configuration = {
+      "name": "MyUserName",
+
+      //"username": "50363",
+      //"password": "pass50363",
+      //"domain": "sip.zadarma.com",
+      //"regServer": "sip.zadarma.com", // Default wildcard
+
+      "username": "50363",
+      "password": "pass50363",
+      "domain": "172.16.104.17",
+      "regServer": "",
+      //"regServer": "172.16.104.17", // Default wildcard
+
+
+      "proxy": null,
+      "transport": "UDP",//null, // Default TCP
+      
+      "regTimeout": 3600, // Default 3600
+      "regHeaders": {
+        //"X-Custom-Header": "Value"
+      },
+      //"regContactParams": ";unique-device-token-id=XXXXXXXXX",
+      "regOnAdd": true,  // Default true, use false for manual REGISTRATION
+
+      service: {
+        ua: "siptest",
+        stun: ['stun.l.google.com:19302', 'stun4.l.google.com:19302']
+      },
+
+      network: {
+        useAnyway: true,           // Default: true
+        useWifi: true,              // Default: true
+        use3g: true,                // Default: false
+        useEdge: true,             // Default: false
+        useGprs: true,             // Default: false
+        useInRoaming: true,        // Default: false
+        useOtherNetworks: true      // Default: false
+      }
+    };
+
+    let state = await this.sEndpoint.start();
+    console.log("sEndpoint started");
+
+    let { accounts, calls, settings, connectivity } = state;
+    console.log("accounts:\n", accounts);
+    console.log("calls:\n", calls);
+    console.log("settings:\n", settings);
+    console.log("connectivity:\n", connectivity);
+
+    try {
+      console.log("endpoint.createAccount");
+      this.account = await this.sEndpoint.createAccount(configuration);
+      console.log("account created", this.account);
+    } catch (err) {
+      console.log("err");
+      console.error(err);
+    }
+
+
+
+
+
+    // Subscribe to endpoint events
+    this.sEndpoint.on("registration_changed", (account) => {
+      console.log("registration_changed", account);
+    });
+    this.sEndpoint.on("connectivity_changed", (online) => {
+      console.log("connectivity_changed", online);
+    });
+    this.sEndpoint.on("call_received", (call) => {
+      console.log("call_received", call);
+      this.call=call;
+      this.onCallReceived(call)
+    });
+    this.sEndpoint.on("call_changed", (call) => {
+      console.log("call_changed", call);
+      this.onCallTerminated(call);
+      //Если из SIP пришел сигнал повесить трубку - шлем intent в наш Dialer
+    });
+    this.sEndpoint.on("call_terminated", (call) => {
+      console.log("call_terminated", call);
+      this.onCallTerminated(call);
+      //Если из SIP пришел сигнал повесить трубку - шлем intent в наш Dialer
+    });
+    this.sEndpoint.on("call_screen_locked", (call) => {
+      console.log("call_screen_locked", call);
+    }); // Android only
+  }
+
+
+  progress = () => {
+    console.log("pjsip->progress");
+  }
+
+  answer = () => {
+    console.log("pjsip->answer");
+    let options = {};
+    let promise = this.endpoint.answerCall(this.call, options);
+    promise.then(() => {
+      console.log('Answer complete');
+    }).catch((e) => {
+      console.error('Answer failed, show error', e);
+    });
+  }
+
+  hangup = () => {
+    console.log("pjsip->hangup");
+    this.endpoint.hangup();
+  }
+
+  destroy = () => {
+    this.endpoint.destroy();
   }
 
   render() {
