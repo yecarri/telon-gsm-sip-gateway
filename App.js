6e513471d3ab78c28952fc9c48e561b3a8fd3a5d
cpodimas@gmail.com <cpodimas@gmail.com>
2020-01-08 22:43:59 +0300

    auto

---
+++ App.js
@@ -0,0 +1,159 @@
+
+import React, { Component } from 'react';
+
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import { Endpoint } from 'react-native-tele'
+import { ReplaceDialer } from 'react-native-replace-dialer'
+
+export default class App extends Component {
+  constructor() {
+    super();
+  }
+  async componentDidMount() {
+
+    let tReplaceDialer = new ReplaceDialer();
+
+    if (!tReplaceDialer.isDefault()) {
+      console.log('Is NOT default dialer, try to set.');
+      if (tReplaceDialer.setDefault()) {
+        console.log('Default dialer sucessfully set.');
+      } else {
+        console.log('Default dialer NOT set');
+      }
+    }
+
+
+    let tEndpoint = new Endpoint();
+    console.log(tEndpoint);
+
+    let state = await tEndpoint.start(); // List of calls when RN context is started, could not be empty because Background service is working on Android
+    let { calls, settings } = state;
+    console.log("calls",calls);
+    console.log("settings",settings);
+
+    // Subscribe to endpoint events
+    // tEndpoint.on("registration_changed", (account) => {}); // TODO
+    // tEndpoint.on("connectivity_changed", (online) => {}); // TODO
+    tEndpoint.on("call_received", (call) => { console.log("call_received",call); });
+    tEndpoint.on("call_changed", (call) => { console.log("call_changed",call);  });
+    tEndpoint.on("call_terminated", (call) => { console.log("call_terminated",call);  });
+    // tEndpoint.on("call_screen_locked", (call) => {  console.log("call_screen_locked",call);  }); // Android only
+
+
+    let options = {
+      headers: {
+        "sim": "1" // TODO
+      }
+    }
+
+    //let call = await tEndpoint.makeCall(destination, options);
+  }
+
+  render() {
+
+
+
+    return (
+      <>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView>
+          <ScrollView
+            contentInsetAdjustmentBehavior="automatic"
+            style={styles.scrollView}>
+            <Header />
+            {global.HermesInternal == null ? null : (
+              <View style={styles.engine}>
+                <Text style={styles.footer}>Engine: Hermes</Text>
+              </View>
+            )}
+            <View style={styles.body}>
+              <View style={styles.sectionContainer}>
+                <Text style={styles.sectionTitle}>Step One</Text>
+                <Text style={styles.sectionDescription}>
+                  Edit <Text style={styles.highlight}>App.js</Text> to change this
+                  screen and then come back to see your edits.
+              </Text>
+              </View>
+              <View style={styles.sectionContainer}>
+                <Text style={styles.sectionTitle}>See Your Changes</Text>
+                <Text style={styles.sectionDescription}>
+                  <ReloadInstructions />
+                </Text>
+              </View>
+              <View style={styles.sectionContainer}>
+                <Text style={styles.sectionTitle}>Debug</Text>
+                <Text style={styles.sectionDescription}>
+                  <DebugInstructions />
+                </Text>
+              </View>
+              <View style={styles.sectionContainer}>
+                <Text style={styles.sectionTitle}>Learn More</Text>
+                <Text style={styles.sectionDescription}>
+                  Read the docs to discover what to do next:
+              </Text>
+              </View>
+              <LearnMoreLinks />
+            </View>
+          </ScrollView>
+        </SafeAreaView>
+      </>
+    );
+  };
+}
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
+
+
